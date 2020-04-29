578090c5d23d60ef993bfdfdfc76f4325877861c
erickpiazza <32956392+erickpiazza@users.noreply.github.com>
2020-04-29 20:11:38 -0300

    corrigido

--- src/store/repositories/types.ts
+++ src/store/repositories/types.ts
@@ -1,16 +1,33 @@
 export enum RepositoriesTypes{
-    LOAD_REQUEST = '@repositories/LOAD_REQUEST',
-    LOAD_SUCCESS = '@repositories/LOAD_SUCCESS',
-    LOAD_FAILURE = '@repositories/LOAD_SUCCESS'
+  LOAD_REQUEST = '@repositories/LOAD_REQUEST',
+  LOAD_SUCCESS = '@repositories/LOAD_SUCCESS',
+  LOAD_FAILURE = '@repositories/LOAD_SUCCESS'
 }
 
+export interface LoadRequestAction {
+type: typeof RepositoriesTypes.LOAD_REQUEST;
+}
+export interface LoadSuccesstAction {
+type: typeof RepositoriesTypes.LOAD_SUCCESS;
+payload: {data:Repository[]}
+}
+export interface LoadFailuretAction {
+type: typeof RepositoriesTypes.LOAD_FAILURE;
+}
+
+
 export interface Repository{
-    id:number
-    name:string
+  id:number
+  name:string
 }
 
 export interface RepositoriesState{
-  readonly data: Repository[]
-  readonly loading: boolean
-  readonly error:boolean
+readonly data: Repository[]
+readonly loading: boolean
+readonly error:boolean
 }
+
+export type RepositoryctionTypes =
+ | LoadRequestAction
+ |LoadSuccesstAction
+ |LoadFailuretAction
